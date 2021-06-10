74119d1c625fb2c54b37b1e76374eb800c4e93af
benjatikki <benjamin.tikki@gmail.com>
2021-06-10 08:46:04 +0200

    Add project files.

---
+++ ApiWithJwtRefreshToken/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace ApiWithJwtRefreshToken.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+
+        [JsonIgnore]
+        public string Password { get; set; }
+
+        [JsonIgnore]
+        public List<RefreshToken> RefreshTokens { get; set; }
+    }
+}
---
+++ ApiWithJwtRefreshToken/Helpers/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ApiWithJwtRefreshToken.Entities;
+
+namespace ApiWithJwtRefreshToken.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
